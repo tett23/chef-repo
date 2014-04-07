3d2f1b9e00e463ebdab895113fb96a35bfd92312
tett23 <tett23@gmail.com>
2014-04-07 21:25:09 +0900

    WIP

--- site-cookbooks/basic/recipes/default.rb
+++ site-cookbooks/basic/recipes/default.rb
@@ -7,8 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w{libxml2-dev}.each do |pkg|
+%w{build-essential libxml2-dev zsh-beta}.each do |pkg|
   package pkg do
     action :install
   end
 end
+
+git '/home/tett23/dotfiles' do
+  action :sync
+  repository 'git@github.com:tett23/dotfiles.git'
+  reference 'master'
+  user 'tett23'
+  group 'tett23'
+end
+
+execute 'install rvm' do
+  command 'curl -sSL https://get.rvm.io | bash'
+  not_if {`which rvm`.match(/not found/)}
+end
+
---
+++ site-cookbooks/common/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: common
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'users'
+
+users_manage 'tett23' do
+  group_id 1000
+  action [:remove, :create]
+end
